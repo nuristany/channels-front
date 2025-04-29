8ac58eae2b0413c3a8cc4f398c2a337c8b9a32da
Qais <nuristanyqais@gmail.com>
2025-04-29 21:27:00 +0430

    added authorization header in useConversation

--- src/hooks/useConversaton.jsx
+++ src/hooks/useConversaton.jsx
@@ -1,17 +1,24 @@
 // hooks/useConversation.js
 import { useQuery } from '@tanstack/react-query';
+import useAuth from "../context/useAuth"
 import axios from 'axios';
 
 export const useConversation = (channelId) => {
+  const {authToken} = useAuth();
   return useQuery({
     queryKey: ['conversation', channelId],
     queryFn: async () => {
       const { data } = await axios.get(
-        `https://channels-backend-production.up.railway.app/api/channels/${channelId}/conversation/`
+        `https://channels-backend-production.up.railway.app/api/channels/${channelId}/conversation/`,
+        {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          }
+        }
       );
       return data;
     },
-    enabled: !!channelId,
+    enabled: !!channelId && !!authToken,
   });
 };
 
