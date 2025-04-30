0b3d6cce90a83e51c571facbb556e995934a660d
Qais <nuristanyqais@gmail.com>
2025-04-30 11:44:54 +0430

    FIXED HANDLE LOGOUT

--- src/components/primaryAppBar/AccountButton.jsx
+++ src/components/primaryAppBar/AccountButton.jsx
@@ -35,7 +35,7 @@ const AccountButton = () => {
         onClose={handleClose}
       >
         <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
+        <MenuItem onClick={handleLogout}>Logout</MenuItem>
       </Menu>
     </Box>
   );
