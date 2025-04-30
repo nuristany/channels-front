9b3f6e4a2cd1dfd66805ca2a6a98ef8ab80d70fa
Qais <nuristanyqais@gmail.com>
2025-04-30 20:21:13 +0430

    fix

--- src/hooks/useMessages.jsx
+++ src/hooks/useMessages.jsx
@@ -27,7 +27,6 @@
 import { useQuery } from '@tanstack/react-query';
 import useAuth from "../context/useAuth"
 import axios from 'axios';
-import API_URL from '../api/api';
 
 
 export const useMessages = (conversationId) => {
@@ -36,7 +35,7 @@ export const useMessages = (conversationId) => {
     queryKey: ['messages', conversationId],
     queryFn: async () => {
       const { data } = await axios.get(
-        `${API_URL}/api/messages/conversation/${conversationId}/`,
+        `https://channels-backend-production.up.railway.app/api/messages/conversation/${conversationId}/`,
         {
           headers: {
             Authorization: `Bearer ${authToken}`,
